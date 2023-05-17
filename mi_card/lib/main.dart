7f19f15d3add54798e4d5146937a6c6c8be2c498
Alex Calovi <alex.calovi98@gmail.com>
2023-05-17 11:03:37 +0200

    mi card app completed

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,34 +11,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mi Card',
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lightBlue[300],
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                width: 100,
-                color: Colors.red
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
               ),
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: ColoredBox(color: Colors.yellow),
+              const Text(
+                "Alex Calovi",
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                "SOFTWARE DEVELOPER",
+                style: TextStyle(
+                    fontSize: 24.0,
+                    color: Colors.lightBlue[100],
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.lightBlue[50]
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(Icons.phone, color: Colors.lightBlue),
+                  title: Text(
+                    "+39 349 345 6401",
+                    style: TextStyle(
+                        color: Colors.lightBlue[700],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0),
                   ),
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: ColoredBox(color: Colors.lime),
-                  )
-                ]
+                ),
               ),
-              Container(width: 100, color: Colors.blue)
+              Card(
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(Icons.email, color: Colors.lightBlue),
+                  title: Text(
+                    "alex.calovi98@gmail.com",
+                    style: TextStyle(
+                        color: Colors.lightBlue[700],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
